65239224bf1220427fe4d15f52b1fe49dece3140
Jake Maguy <jakemaguy@gmail.com>
2019-09-28 15:49:33 -0400

    Fixed up recipe list page and created model for recipes

--- src/app/components/recipe_book/recipe_book-list/recipe_book.list.ts
+++ src/app/components/recipe_book/recipe_book-list/recipe_book.list.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Recipe } from '../../../models/recipe.model';
 
 @Component({
     selector: 'app-recipe-book-list',
@@ -6,12 +7,12 @@ import { Component, OnInit } from '@angular/core';
 })
 
 export class RecipeBookListComponent implements OnInit {
-    recipes = [
-        'Chicken Parm',
-        'Ceasar Salad',
-        'Filet Mignon',
-        'Lasagna'
+    recipes: Recipe[] = [
+        new Recipe('Chicken Parmesan',
+        'Tender Pan Fried Chicken Breasts topped with tomato sauce.',
+        'https://www.cookingclassy.com/wp-content/uploads/2013/02/chicken-parmesan-16.jpg')
     ];
+
     constructor() { }
 
     ngOnInit() { }
