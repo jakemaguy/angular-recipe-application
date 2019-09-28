bb7f700a0bf34051144872bd07f48738accc0f1d
Jake Maguy <jakemaguy@gmail.com>
2019-09-28 16:26:56 -0400

    Created a model for ingredients

---
+++ src/app/models/ingredient.model.ts
@@ -0,0 +1,5 @@
+export class Ingredient {
+    // Accessors in arguments create the variables
+    // same as defining these variables in the class
+    constructor(public name: string, public amount: number) {}
+}
