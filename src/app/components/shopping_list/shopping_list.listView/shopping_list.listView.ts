f12bb3afbf1ace06be281139ccc99ebd6ecb0a81
Jake Maguy <jakemaguy@gmail.com>
2019-09-28 16:32:43 -0400

    Edited shopping list HTML to output ingredient objects

--- src/app/components/shopping_list/shopping_list.listView/shopping_list.listView.ts
+++ src/app/components/shopping_list/shopping_list.listView/shopping_list.listView.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {  Ingredient } from '../../../models/ingredient.model';
 
 @Component({
     selector: 'app-shopping-list-listview',
@@ -6,11 +7,9 @@ import { Component, OnInit } from '@angular/core';
 })
 
 export class ShoppingListViewComponent implements OnInit {
-    shoppingList = [
-        'carrots',
-        'lettuce',
-        'avacados',
-        'cheetos'
+    ingredients: Ingredient[] = [
+        new Ingredient('Avacados', 2),
+        new Ingredient('Tomatoes', 5)
     ];
     constructor() { }
 
