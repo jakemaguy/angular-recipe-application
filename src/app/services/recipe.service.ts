02a2892f714c30b2a1b57922baf7cf894d15dd73
Jake Maguy <jakemaguy@gmail.com>
2019-10-19 13:23:54 -0400

    Created recipe + shopping list services

---
+++ src/app/services/recipe.service.ts
@@ -0,0 +1,21 @@
+import { Injectable } from '@angular/core';
+import { Recipe } from '../models/recipe.model';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class RecipeService {
+  private recipes: Recipe[] = [
+    new Recipe('Chicken Parmesan',
+      'Tender Pan Fried Chicken Breasts topped with tomato sauce.',
+      'https://www.cookingclassy.com/wp-content/uploads/2013/02/chicken-parmesan-16.jpg'),
+
+    new Recipe('Buffalo Mac And Cheese',
+      'Delicious Buf mac.  Perfect for any party.',
+      'https://spicysouthernkitchen.com/wp-content/uploads/Buffalo-Chicken-Mac-and-Cheese-8.jpg')
+  ];
+
+  getRecipes() {
+    return this.recipes.slice(); // returns new array, exact copy
+  }
+}
